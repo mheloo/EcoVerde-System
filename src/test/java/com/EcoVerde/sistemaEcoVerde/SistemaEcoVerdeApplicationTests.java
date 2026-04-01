6607b5113d7c66424faad85e7634a3674a2c8106
Marcelo <killostynfl@gmail.com>
2026-03-31 21:33:35 -0300

    feat: initial config

---
+++ src/test/java/com/EcoVerde/sistemaEcoVerde/SistemaEcoVerdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.EcoVerde.sistemaEcoVerde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEcoVerdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
