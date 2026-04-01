af83e127e3dd17c21c7906fb813e58c60bf22aea
Marcelo <killostynfl@gmail.com>
2026-03-31 21:33:21 -0300

    feat: initial config

---
+++ src/main/java/com/EcoVerde/sistemaEcoVerde/SistemaEcoVerdeApplication.java
@@ -0,0 +1,13 @@
+package com.EcoVerde.sistemaEcoVerde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaEcoVerdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaEcoVerdeApplication.class, args);
+	}
+
+}
