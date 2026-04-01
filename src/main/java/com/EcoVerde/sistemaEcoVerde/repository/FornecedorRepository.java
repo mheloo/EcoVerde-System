e255bb0768f56c0dc8ab1ec2b942ef1350d4dda8
Marcelo <killostynfl@gmail.com>
2026-03-31 22:13:22 -0300

    feat: update Repository by (Fornecedor)

--- src/main/java/com/EcoVerde/sistemaEcoVerde/repository/FornecedorRepository.java
+++ src/main/java/com/EcoVerde/sistemaEcoVerde/repository/FornecedorRepository.java
@@ -3,6 +3,10 @@ package com.EcoVerde.sistemaEcoVerde.repository;
 import com.EcoVerde.sistemaEcoVerde.domain.fornecedor.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FornecedorRepository extends JpaRepository <Fornecedor, Long> {
+    Optional<Fornecedor> findByDocumento(String documento);
+    Optional<Fornecedor> findByNome(String nome);
 }
 
