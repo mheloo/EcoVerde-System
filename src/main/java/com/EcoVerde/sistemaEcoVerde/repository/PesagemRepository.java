05d0eb3e573b4f0c8ada9f78d3ce20027f638b07
Marcelo <killostynfl@gmail.com>
2026-04-07 21:13:33 -0300

    feat: create List Pesagem in Repository

--- src/main/java/com/EcoVerde/sistemaEcoVerde/repository/PesagemRepository.java
+++ src/main/java/com/EcoVerde/sistemaEcoVerde/repository/PesagemRepository.java
@@ -2,7 +2,16 @@ package com.EcoVerde.sistemaEcoVerde.repository;
 
 import com.EcoVerde.sistemaEcoVerde.domain.fornecedor.Pesagem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Repository
 
 public interface PesagemRepository extends JpaRepository <Pesagem, Long> {
-    
+
+    //Busca todas as pesagens entre duas datas/horas
+    List<Pesagem> findByDataRegistroBetween(LocalDateTime inicio, LocalDateTime fim);
+
 }
