6a444e85bd22e942e9a9b5f69c11e2816a543720
Marcelo <killostynfl@gmail.com>
2026-04-07 21:12:20 -0300

    feat: update @GetMapping List PesagemController

--- src/main/java/com/EcoVerde/sistemaEcoVerde/controller/PesagemController.java
+++ src/main/java/com/EcoVerde/sistemaEcoVerde/controller/PesagemController.java
@@ -1,10 +1,13 @@
 package com.EcoVerde.sistemaEcoVerde.controller;
 
+import com.EcoVerde.sistemaEcoVerde.domain.fornecedor.Pesagem;
 import com.EcoVerde.sistemaEcoVerde.dto.NotaPesagemDTO;
 import com.EcoVerde.sistemaEcoVerde.service.PesagemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -25,5 +28,12 @@ public class PesagemController {
         );
 
         return ResponseEntity.ok(nota);
+    }
+
+    @GetMapping("/relatorio-hoje")
+    public ResponseEntity<List<Pesagem>> getRelatorioHoje() {
+        List<Pesagem> lista = service.listarPesagemDoDia();
+
+        return ResponseEntity.ok(lista);
     }
 }
